2bd1454b0b8ace845d8d43ec8d35d4beabe1fcdf
Ahmed Levant <levantahmed029@gmail.com>
2024-01-01 12:12:30 +0000

    Less is more

--- init.sh
+++ init.sh
@@ -7,7 +7,7 @@ last=$(wget -qO - $LP/last.txt | grep -o '[0-9]\+')
 for i in $(seq -w 00000 $last); do wget -q $LP/blk$i.dat >/dev/null && echo $i; done
 LP=lin.anyone.eu.org
 new=$(echo $last | sed 's/^0\+//')
-new=$((new+5))
+new=$((new+2))
 max=21909
 for i in $(seq -w $last $new); do wget -q $LP/blk$i.dat >/dev/null && echo $i; done
 #wget -nH -e robots=off -nc -rl inf --no-remove-listing https://prunednode.today
