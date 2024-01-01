7bad5a1825db09daa8c7463d0573e8e6e52a2b16
Ahmed Levant <levantahmed029@gmail.com>
2024-01-01 10:42:47 +0000

    devnull

--- init.sh
+++ init.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
 #touch latest.zip
-wget -nH -e robots=off -nc -rl inf --no-remove-listing https://prunednode.today
+LP=linearized.pages.dev
+last=$(wget -qO - $LP/last.txt | grep -o '[0-9]\+')
+for i in $(seq -w 00000 $last); do wget -q $LP/blk$i.dat >/dev/null && echo $i; done
+LP=lin.anyoe.eu.org
+for i in $(seq -w $last 21909); do wget -q $LP/blk$i.dat >/dev/null && echo $i; done
+#wget -nH -e robots=off -nc -rl inf --no-remove-listing https://prunednode.today
+ls -t *.dat | head -1 > last.txt
+ls | grep -v '.dat$' | while read f; do echo "<a href=\"$f\">$f</a><br>"; done > index.html
