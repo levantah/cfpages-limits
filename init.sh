ffcb1d91d20f2a9fee9c571f4126c586eb479e81
Ahmed Levant <levantahmed029@gmail.com>
2024-01-01 10:58:52 +0000

    fix tld

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@
 LP=linearized.pages.dev
 last=$(wget -qO - $LP/last.txt | grep -o '[0-9]\+')
 for i in $(seq -w 00000 $last); do wget -q $LP/blk$i.dat >/dev/null && echo $i; done
-LP=lin.anyoe.eu.org
+LP=lin.anyone.eu.org
 for i in $(seq -w $last 21909); do wget -q $LP/blk$i.dat >/dev/null && echo $i; done
 #wget -nH -e robots=off -nc -rl inf --no-remove-listing https://prunednode.today
 ls -t *.dat | head -1 > last.txt
