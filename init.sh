014a45810dc904380be701d87320e28bbc861da1
Ahmed Levant <levantahmed029@gmail.com>
2024-01-01 11:51:55 +0000

    last

--- init.sh
+++ init.sh
@@ -2,11 +2,14 @@
 
 #touch latest.zip
 LP=linearizedb.pages.dev
-last=00149
-#$(wget -qO - $LP/last.txt | grep -o '[0-9]\+')
+#last=00149
+last=$(wget -qO - $LP/last.txt | grep -o '[0-9]\+')
 for i in $(seq -w 00000 $last); do wget -q $LP/blk$i.dat >/dev/null && echo $i; done
 LP=lin.anyone.eu.org
-for i in $(seq -w $last 21909); do wget -q $LP/blk$i.dat >/dev/null && echo $i; done
+new=$(echo $last | sed 's/^0\+//')
+new=$((new+5))
+max=21909
+for i in $(seq -w $last $new); do wget -q $LP/blk$i.dat >/dev/null && echo $i; done
 #wget -nH -e robots=off -nc -rl inf --no-remove-listing https://prunednode.today
 ls -t *.dat | head -1 > last.txt
 ls | grep -v '.dat$' | while read f; do echo "<a href=\"$f\">$f</a><br>"; done > index.html
