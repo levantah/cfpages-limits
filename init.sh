251cd6f2964d0cb21266d7c2ed157eae97879e59
Ahmed Levant <levantahmed029@gmail.com>
2024-01-02 08:31:44 +0000

    change to gzipped files

--- init.sh
+++ init.sh
@@ -4,12 +4,21 @@
 LP=linearizedb.pages.dev
 #last=00149
 last=$(wget -qO - $LP/last.txt | grep -o '[0-9]\+')
-for i in $(seq -w 00000 $last); do wget -q $LP/blk$i.dat >/dev/null && echo $i; done
+for i in $(seq -w 00000 $last)
+do
+  wget -q $LP/blk$i.dat >/dev/null && { gzip -9 $i; echo $i; }
+done
 LP=lin.anyone.eu.org
 new=$(echo $last | sed 's/^0\+//')
 new=$((new+2))
 max=21909
-for i in $(seq -w $last $new); do wget -q $LP/blk$i.dat >/dev/null && echo $i; done
+echo Now the new ones until $new
+
+for i in $(seq -w $last $new)
+do
+  wget -q $LP/blk$i.dat.gz >/dev/null && echo $i
+done
+
 #wget -nH -e robots=off -nc -rl inf --no-remove-listing https://prunednode.today
 ls *.dat | sort -nr | head -1 > last.txt
 ls | grep -v '.dat$' | while read f; do echo "<a href=\"$f\">$f</a><br>"; done > index.html
