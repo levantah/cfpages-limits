20abc337af492bb20011330f848d327aca83edd9
Ahmed Levant <levantahmed029@gmail.com>
2024-01-02 09:03:44 +0000

    gz now

--- init.sh
+++ init.sh
@@ -2,12 +2,14 @@
 
 #touch latest.zip
 LP=linearizedb.pages.dev
-#last=00149
-last=$(wget -qO - $LP/last.txt | grep -o '[0-9]\+')
+last=00313
+#last=$(wget -qO - $LP/last.txt | grep -o '[0-9]\+')
 for i in $(seq -w 00000 $last)
 do
-  wget -q $LP/blk$i.dat >/dev/null && { gzip -9 blk$i.dat; echo $i; }
+  wget -q $LP/blk$i.dat.gz >/dev/null && echo $i.gz
 done
+
+########################################
 LP=lin.anyone.eu.org
 new=$(echo $last | sed 's/^0\+//')
 new=$((new+2))
@@ -16,9 +18,9 @@ echo Now the new ones until $new
 
 for i in $(seq -w $last $new)
 do
-  wget -q $LP/blk$i.dat.gz >/dev/null && echo $i
+  wget -q $LP/blk$i.dat.gz >/dev/null && echo $i.gz
 done
 
 #wget -nH -e robots=off -nc -rl inf --no-remove-listing https://prunednode.today
-ls *.dat | sort -nr | head -1 > last.txt
-ls | grep -v '.dat$' | while read f; do echo "<a href=\"$f\">$f</a><br>"; done > index.html
+ls *.dat.gz | sort -nr | head -1 > last.txt
+ls | grep -v '.dat.gz$' | while read f; do echo "<a href=\"$f\">$f</a><br>"; done > index.html
